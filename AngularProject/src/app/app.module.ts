06bbb23126a5d925b7696dd1ed56157bad0766ad
alexisloor <caalloor@espol.edu.ec>
2023-08-07 00:17:58 -0500

    files

---
+++ AngularProject/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { Top3MaleComponent } from './top3-male/top3-male.component';
+import { Top3FemComponent } from './top3-fem/top3-fem.component';
+import { GraficoComponent } from './grafico/grafico.component';
+
+import { HttpClientModule } from  '@angular/common/http';
+import { PromedioTiempoManComponent } from './promedio-tiempo-man/promedio-tiempo-man.component';
+import { PromedioTiempoFemComponent } from './promedio-tiempo-fem/promedio-tiempo-fem.component';
+
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { TitulostotalComponent } from './titulostotal/titulostotal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    AboutComponent,
+    Top3MaleComponent,
+    Top3FemComponent,
+    GraficoComponent,
+    PromedioTiempoManComponent,
+    PromedioTiempoFemComponent,
+    TitulostotalComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CanvasJSAngularChartsModule,
+    AppRoutingModule,
+    HttpClientModule  //Registro de módulo importado
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
